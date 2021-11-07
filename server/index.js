22ec8774d3fb84dd5da13695108a3832def29e6e
Gereltuya Munkhzaya <gegiimz96@gmail.com>
2021-11-07 18:45:00 +0800

    change cors

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ import cors from "cors";
 import postRoutes from "./routes/post.js";
 const app = express();
 
-// reached at localhost:5000/posts
-app.use("/posts", postRoutes);
 // image size limit
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// reached at localhost:5000/posts
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
 	"mongodb+srv://memoriesproject:memoriesproject@cluster0.voumc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
